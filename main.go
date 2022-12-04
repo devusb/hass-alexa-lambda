27bb4079cce5c8daded07d62b0dfcd8579cbdba3
Morgan Helton <mhelton@gmail.com>
2022-12-03 21:28:30 -0600

    add tailscale to function

--- main.go
+++ main.go
@@ -9,24 +9,46 @@ import (
 		"net/http"
 		"os"
 		"fmt"
+		"tailscale.com/tsnet"
+		"time"
 )
 
 func HandleRequest(ctx context.Context, event json.RawMessage) (json.RawMessage, error) {
 	url := os.Getenv("BASE_URL")
-
-	var f interface{}
-	_  = json.Unmarshal(event, &f)
-	m := f.(map[string]interface{})["directive"].(map[string]interface{})["endpoint"].(map[string]interface{})["scope"].(map[string]interface{})
+	token_env, token_present := os.LookupEnv("TOKEN")
 
 	auth := "Bearer "
-	auth += fmt.Sprint(m["token"])
+	// use token from environment if present
+	if token_present { 
+		auth += token_env
+	} else {
+		var f interface{}
+		_  = json.Unmarshal(event, &f)
+		m := f.(map[string]interface{})["directive"].(map[string]interface{})["endpoint"].(map[string]interface{})["scope"].(map[string]interface{})
+		auth += fmt.Sprint(m["token"])
+	}
 
 	url += "/api/alexa/smart_home"
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(event))
 	req.Header.Set("Authorization", auth)
 	req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{}
+	s := &tsnet.Server{
+		Dir: "/tmp",
+		Hostname: "hass-alexa",
+		Ephemeral: true,
+	}
+
+	defer s.Close()
+
+	_, _ = s.LocalClient()
+	client := &http.Client{
+		Transport: &http.Transport{
+			DialContext: s.Dial, // use the tailscale dialer
+		},
+	}
+	time.Sleep(1500*time.Millisecond) // wait for tailnet connection to come up
+	
 	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
