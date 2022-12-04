db2954c860cecff98004c09b70e29094aa5a5a3c
Morgan Helton <mhelton@gmail.com>
2022-12-04 16:30:05 -0600

    allow setting tailscale startup delay via environment variable

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"tailscale.com/tsnet"
 	"time"
 )
@@ -16,6 +17,16 @@ import (
 func HandleRequest(ctx context.Context, event json.RawMessage) (json.RawMessage, error) {
 	url := os.Getenv("BASE_URL")
 	token_env, token_present := os.LookupEnv("TOKEN")
+	sleep_env, sleep_present := os.LookupEnv("TS_DELAY")
+
+	// allow setting tailscale startup delay via environment variable
+	var ts_delay time.Duration
+	if sleep_present {
+		sleep_env_int, _ := strconv.Atoi(sleep_env)
+		ts_delay = time.Duration(sleep_env_int) * time.Millisecond
+	} else {
+		ts_delay = 1500 * time.Millisecond
+	}
 
 	auth := "Bearer "
 	// use token from environment if present
@@ -28,17 +39,18 @@ func HandleRequest(ctx context.Context, event json.RawMessage) (json.RawMessage,
 		auth += fmt.Sprint(m["token"])
 	}
 
+	// collect connection details for Home Assistant
 	url += "/api/alexa/smart_home"
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(event))
 	req.Header.Set("Authorization", auth)
 	req.Header.Set("Content-Type", "application/json")
 
+	// initiate tailscale connection
 	s := &tsnet.Server{
 		Dir:       "/tmp",
 		Hostname:  "hass-alexa",
 		Ephemeral: true,
 	}
-
 	defer s.Close()
 
 	_, _ = s.LocalClient()
@@ -47,7 +59,7 @@ func HandleRequest(ctx context.Context, event json.RawMessage) (json.RawMessage,
 			DialContext: s.Dial, // use the tailscale dialer
 		},
 	}
-	time.Sleep(1500 * time.Millisecond) // wait for tailnet connection to come up
+	time.Sleep(ts_delay) // wait for tailnet connection to come up
 
 	resp, err := client.Do(req)
 	if err != nil {
